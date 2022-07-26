a560f7086418e67d18b758dd0c0fe2fcb33a1bb5
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-07-26 13:35:50 -0400

    Create compare.py

---
+++ compare.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+
+if a > b:
+  print(str(a) + " is larger than " + str(b))
+elif a < b:
+  print(str(b) + " is larger than " + str(a))
+else:
+  print(str(a) + " is equal to " + str(b))
+  
